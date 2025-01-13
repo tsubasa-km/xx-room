afe70be6f03b2c2a8f34eeea0231f69fd21eb1be
SUGIMOTO Takuma <2577371+sixpetals@users.noreply.github.com>
2025-01-13 23:46:45 +0900

    JoinRoomの中身を実装

--- frontend/app/join-room/page.tsx
+++ frontend/app/join-room/page.tsx
@@ -1,5 +1,33 @@
-export default function JoinRoom() { 
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function JoinRoom() {
+    useState("");
+
+    const router = useRouter();
+
+    const handleJoinRoom = async () => {
+        // ここで部屋に参加する処理を書く
+        // ダミーデータ
+        const data = {
+            roomId: "dummy-room-id",
+        }
+
+        router.push(`/room/${data.roomId}`);
+    }
+
     return (
-        <div></div>
+        <div>
+            <h1>部屋に参加</h1>
+            <div>
+                <label htmlFor="user-name">ユーザー名</label>
+                <input type="text" id="user-name" />
+            </div>
+            <div>
+                <label htmlFor="room-id">部屋ID</label>
+                <input type="text" id="room-id" />
+            </div>
+            <button onClick={handleJoinRoom}>参加</button>
+        </div>
     );
 }
