6c976b3dd3451db67e60d13ca270198117ab8c64
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-01-16 14:57:01 +0900

    ホームページのボタンスタイルを更新し、レイアウトを中央揃えに変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button, Container } from "@mui/material";
+"use client";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <div>
-      <Container>
-        <Button variant="contained" href="/create-room">部屋を作成</Button>
-        <Button variant="contained" href="/join-room">部屋に参加</Button>
-      </Container>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Button variant="contained" href="/create-room" style={{ marginBottom: '20px', fontSize:"1.8em", width:"300px"}}>部屋を作成</Button>
+      <Button variant="contained" href="/join-room" color="inherit" style={{fontSize:"1.5em", width:"300px"}}>部屋に参加</Button>
     </div>
   );
 }
